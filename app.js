9d98e731ed4bc3ec9320329fb832e457573c4e95
uroojtariq <35533607+Uroojtariq02@users.noreply.github.com>
2020-06-21 17:52:00 +0500

    Assignment 6

---
+++ app.js
@@ -0,0 +1,35 @@
+// Chapter 21 to 25
+        // Question # 1
+                //  <script>
+                //   var fname = prompt("Enter your first name: ", "");
+                //   var lname = prompt("Enter your last name: ", "");
+                //   var fullName = fname + lname;
+                //   document.write("<h1>Hello, " + fullName + "!</h1>");
+                //</script>
+
+        // Question # 2
+                //    <script>
+                //    var phone = prompt("What is your  favorite mobile phone model? : ", "");
+                //    var n = phone.length;
+                //   document.write("<h1>My Favorite mobile model is, " + phone + "!</h1>");
+                //    document.write("<h2>The Length Of String is, " + n + "!</h2>");
+                //    </script>
+
+        // Question # 3
+                //<script>
+                //       var str = "Pakistan";
+                //       document.write("<h1>String is = " + str + "</h1>");
+                //       var n = str.indexOf("n");
+                //       document.write("<h2>The index Of n is, " + n + "</h2>");
+                //  </script>
+
+        // Question # 4
+                //<script>
+                //       var str = "Hello World";
+                //       document.write("<h1>String = " + str + "</h1>");
+                //       var n = str.lastIndexOf("l");
+                //       document.write("<h2>Last index of 'l' = " + n + "</h2>");
+                //  </script>
+
+
+
